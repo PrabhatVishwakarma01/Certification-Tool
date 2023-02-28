1bb0c6b2013ad122cfa551fa24f7057118cfc040
Kunal Mishra <mishra2000kunal@gmail.com>
2023-02-28 20:04:37 +0530

    Added Validation for login page

--- Shared/LoginViewModel.cs
+++ Shared/LoginViewModel.cs
@@ -16,18 +16,23 @@ namespace Tool.Shared
             set;
         }
         [Required]
-        [Display(Name = "Email")]
+        [EmailAddress]
         public string Email
         {
             get;
             set;
         }
         [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [Display(Name = "Password")]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", 
+            ErrorMessage = "Passwords must be at least 8 characters and contain the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string Password
         {
             get;
             set;
         }
+        
     }
 }
