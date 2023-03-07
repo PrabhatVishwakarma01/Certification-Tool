2fb8c35177011a1e557a4a1ba2e8e9d917a7723d
Kunal Mishra <mishra2000kunal@gmail.com>
2023-03-07 19:41:58 +0530

    Updated Password field validation

--- Shared/LoginViewModel.cs
+++ Shared/LoginViewModel.cs
@@ -26,7 +26,6 @@ namespace Tool.Shared
         }
         [Required]
         [DataType(DataType.Password)]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [Display(Name = "Password")]
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
             ErrorMessage = "Passwords must be at least 8 characters and contain the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
