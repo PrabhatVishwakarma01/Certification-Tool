6a610847c0132f937bb6274fb7da5a619b85ce4e
Mayur Mandape <mayur_mandape@persistent.com>
2023-03-24 15:26:11 +0530

    RemovedAddingOfDuplicateQuestionInAddQuestionsPage

--- Server/Controllers/QuestionController.cs
+++ Server/Controllers/QuestionController.cs
@@ -3,69 +3,61 @@ using Tool.Server.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Tool.Server.Controllers
-{
+namespace Tool.Server.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class QuestionController : ControllerBase
-    {
+    public class QuestionController : ControllerBase {
         private readonly IQuestionService _questionService;
-        public QuestionController(IQuestionService questionService)
-        {
+        public QuestionController(IQuestionService questionService) {
             _questionService = questionService;
         }
-        
+
         [HttpGet]
-        public async Task<List<QuestionModel>> GetAll()
-        {
+        public async Task<List<QuestionModel>> GetAll() {
             return await _questionService.GetAllQuestionModel();
         }
 
         [HttpGet("{id}")]
-        public async Task<QuestionModel> Get(int id)
-        {
+        public async Task<QuestionModel> Get(int id) {
             return await _questionService.GetQuestionModel(id);
         }
 
-        [HttpPost]
-        public async Task<QuestionModel> AddQuestionModel([FromBody] QuestionModel question)
-        {
-           return await _questionService.AddQuestionModel(question);
-        }
+        //[HttpPost]
+        //public async Task<QuestionModel> AddQuestionModel([FromBody] QuestionModel question)
+        //{
+
+        //   return await _questionService.AddQuestionModel(question);
+        //}
 
         [HttpDelete("{id}")]
-        public async Task<bool> DeleteQuestionModel(int id)
-        {
+        public async Task<bool> DeleteQuestionModel(int id) {
             await _questionService.DeleteQuestionModel(id); return true;
         }
 
         [HttpPut("{id}")]
-        public async Task<bool> UpdateQuestionModel(int id, [FromBody] QuestionModel Object)
-        {
+        public async Task<bool> UpdateQuestionModel(int id, [FromBody] QuestionModel Object) {
             await _questionService.UpdateQuestionModel(id, Object); return true;
         }
-// POST api/<QuestionController>
-        public async Task<bool> Post([FromBody] QuestionModel question)
-        {
+
+        // POST api/<QuestionController>
+        [HttpPost]
+        public async Task<bool> Post([FromBody] QuestionModel question) {
             Console.WriteLine("hlelloo");
 
             // Check if the question already exists in the database
             QuestionModel existingQuestion = await _questionService.GetQuestionByTextAsync(question.QuestionText);
-            if (existingQuestion != null)
-            {
+            if (existingQuestion != null) {
                 // Quiz already exists, return false to indicate failure
                 return false;
             }
 
             // Quiz doesn't exist, add it to the database
             QuestionModel newQuestion = await _questionService.AddQuestion(question);
-            if (newQuestion != null)
-            {
+            if (newQuestion != null) {
                 // Quiz added successfully, return true to indicate success
                 return true;
             }
-            else
-            {
+            else {
                 // Quiz could not be added, return false to indicate failure
                 return false;
             }
